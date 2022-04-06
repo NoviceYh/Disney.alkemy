4e1d898ffbc877048934c4785e03a49934a80225
NoviceYh <aguirreee21@gmail.com>
2022-04-06 18:02:04 -0300

    Initial commit

---
+++ src/main/java/disney/alkemy/DisneyAlkemyApplication.java
@@ -0,0 +1,13 @@
+package disney.alkemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisneyAlkemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisneyAlkemyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/disney/alkemy/DisneyAlkemyApplicationTests.java
@@ -0,0 +1,13 @@
+package disney.alkemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisneyAlkemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
